2a5fee9cad9af6b421add38336760c4f762fff98
Iudy <lucasiudyrech20@gmail.com>
2026-09-23 11:04:14 -0300

    javascript

---
+++ js/1.js
@@ -0,0 +1,10 @@
+console.log("Olá mundo");
+// Declarar variaveis em JS.
+i=0; //variavel global var
+var nome="Lucas";
+nome=1; //define variavel global
+let numero; //define local por bloco
+const numeroPI = 3.141516;
+
+console.log("Variavel i:"+typeof(i))
+console.log("Variavel nome:"+typeof(nome))
